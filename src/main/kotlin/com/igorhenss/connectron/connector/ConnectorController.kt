ae66ad7d31b8c0ebb61373617eff45dcba6f4967
Igor Silva <igorhenss@gmail.com>
2024-04-03 21:54:44 -0300

    #0009 - Updating route.

--- src/main/kotlin/com/igorhenss/connectron/connector/ConnectorController.kt
+++ src/main/kotlin/com/igorhenss/connectron/connector/ConnectorController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/connect")
+@RequestMapping("/process")
 class ConnectorController(private val service: ConnectorService) {
 
     @PostMapping
